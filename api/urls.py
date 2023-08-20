66c4297105d2c7450f4132e31a906a1181650f4d
Fahad <fhdnaufal@gmail.com>
2023-08-20 14:41:48 +0530

    Commited before altering user model

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,15 @@
 from api.views import *
 from django.urls import path
+from api.views import MyTokenObtainPairView
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 
 urlpatterns=[   
     path('rooms/',rooms,name='rooms'),
     path('room/<int:pk>/',roomViewApi,name='room-api'),
+    path('current-user/',getCurrentUser,name='current-user-api'),
+    path('message/<int:pk>/',create_message,name='room-message-api'),
     path('create-room/',create_room,name='creat-room-api'),
     path('update-room/<int:pk>/',update_room,name='update-room-api'),
     path('delete-room/<int:pk>/',delete_room,name='delete-room-api'),
@@ -13,4 +19,6 @@ urlpatterns=[
     path('user/<int:pk>/',user_profile,name='user-profile-api'),
     path('activity/',activities_view,name='activity-api'),
     path('topics/',topics_view,name='topics-api'),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,40 +1,69 @@
 from django.shortcuts import render,redirect
 from django.db.models import Q
+import random
 from django.contrib.auth.models import User
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from django.contrib.auth import login,logout,authenticate
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from .models import Room,Topic,Message
 from api.serializers import RoomSerializer,UserSerializer,MessageSerializer,TopicSerializer
 from .forms import EditUserForm
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 # Create your views here.
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['username'] = user.username
+        return token
+    
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class=MyTokenObtainPairSerializer
 
 @api_view(['GET'])
 def rooms(request):
     q=request.GET.get('q') if request.GET.get('q') else ''
     rooms=Room.objects.filter(Q(name__icontains=q)| Q(host__username__icontains=q)| Q(topic__name__icontains=q)|Q(description__icontains=q))
+
+    # user=if (request.user.id) return User.objects.get(pk=request.user.id) else None
+
     serializer=RoomSerializer(rooms,many=True)
+    def get_random_users(count):
+    # Get the total number of users in the User model
+        total_users = User.objects.count()
+        # Generate a list of random indexes without repetition
+        random_indexes = random.sample(range(total_users), min(count, total_users))
+        # Fetch users at the random indexes
+        random_users = User.objects.filter(pk__in=random_indexes)
+        return random_users
+    random_users = get_random_users(4)
+
     topics=Topic.objects.all()[:5]
     topics_ser=TopicSerializer(topics,many=True)
     activities=Message.objects.all()[:3]
     activities_ser=MessageSerializer(activities,many=True)
-    data={'rooms':serializer.data,'topics':topics_ser.data,'messages':activities_ser.data}
+    discover_people=UserSerializer(random_users,many=True)
+    data={'rooms':serializer.data,'topics':topics_ser.data,'messages':activities_ser.data,'discover_users':discover_people.data}
     return Response(data)
 
-@api_view(['GET','POST'])
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getCurrentUser(request):
+    user=User.objects.get(id=request.user.id)
+    serializer=UserSerializer(user)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
 def roomViewApi(request,pk):
     roomItem=Room.objects.get(pk=pk)
-    if request.method == 'POST':
-        message=Message.objects.create(
-            user=request.user,
-            room=roomItem,
-            body=request.data.get('message')
-        )
-        roomItem.participants.add(request.user)
-        message.save()
-        return redirect('room-api',roomItem.id)  #return to the specific room where message was added
     room_messages=roomItem.message_set.all().order_by('-created')
     room_messages_serializer=MessageSerializer(room_messages,many=True)
     room_serializer=RoomSerializer(roomItem)
@@ -45,7 +74,24 @@ def roomViewApi(request,pk):
     }
     return Response(data)
 
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def create_message(request,pk):
+    roomItem=Room.objects.get(pk=pk)
+    message=Message.objects.create(
+        user=request.user,
+        room=roomItem,
+        body=request.data.get('message')
+    )
+    roomItem.participants.add(request.user)
+    message.save()
+    serializer=MessageSerializer(message)
+
+    return Response(serializer.data)  #return to the specific room where message was added
+
+
 @api_view(["POST"])
+@permission_classes([IsAuthenticated])
 def create_room(request):
     print(request.user)
     if not request.user.is_authenticated:
@@ -65,6 +111,7 @@ def create_room(request):
 
 
 @api_view(["POST"])
+@permission_classes([IsAuthenticated])
 def update_room(request,pk):
     if not request.user.is_authenticated:
         return Response('unauthorized') 
@@ -76,6 +123,7 @@ def update_room(request,pk):
 
 
 @api_view(["GET"])
+@permission_classes([IsAuthenticated])
 def delete_room(request,pk):
     if not request.user.is_authenticated:
         return Response('unauthorized')
@@ -90,7 +138,8 @@ def login_user(request):
     data=request.data
     if request.user.is_authenticated:
         return Response('active')   
-    username=data.get('username')
+    username=data.get('username') 
+
     password=data.get('password')
 
     try:
@@ -132,7 +181,6 @@ def logout_user(request):
 @api_view(["GET"])
 def user_profile(request,pk):
     user=User.objects.get(pk=pk)
-    print(user)
     serializer=UserSerializer(user)
 
     topics=Topic.objects.all()
@@ -149,6 +197,7 @@ def user_profile(request,pk):
     return Response(user_profile_data)
 
 @api_view(["POST"])
+@permission_classes([IsAuthenticated])
 def edit_profile(request):
     data=request.data
     user=request.user
